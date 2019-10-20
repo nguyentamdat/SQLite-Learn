482cd88054678ae7e15478c9a1949468d1529afc
nguyentamdat <nguyentamdat@gmail.com>
2019-10-20 17:40:36 +0700

    create 2 db

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-console */
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./db/test.db', (err) => {
+const db1 = new sqlite3.Database('./db/test.db', (err) => {
   if (err) {
     return console.error(err.message);
   }
   return console.log('Connected to test db');
+});
+
+const db2 = new sqlite3.Database('./db/test2.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  return console.log('Connected to test2 db');
 });
 
 const sql = 'SELECT Id id FROM employee';
@@ -14,14 +21,35 @@ function checkUpdate(newData, currentData) {
   return newData.filter((item1) => !currentData.includes(item1));
 }
 
-db.all(sql, [], (err, rows) => {
-  if (err) { return console.error(err.message); }
-  return console.log('checkUpdate(rows, rows): ', checkUpdate(rows, rows));
-});
+async function checkDatabase() {
+  try {
+    const myDb = db1.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      return rows;
+    });
+    const newDb = db2.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      return rows;
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+}
 
-db.close((err) => {
+db1.close((err) => {
   if (err) {
     return console.error(err.message);
   }
   return console.log('Closed test db');
+});
+
+db2.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  return console.log('Closed test2 db');
 });
